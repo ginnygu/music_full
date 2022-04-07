ff1d5d3a771729d3b39573fcb07fe5b08a4a8ebb
ginnygu <ginnygu@gmail.com>
2022-04-07 12:52:08 -0400

    updated create account controller to save to database

--- server/routes/accounts/controller/accountController.ts
+++ server/routes/accounts/controller/accountController.ts
@@ -1,5 +1,6 @@
 import Account from "../model/Account";
 import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 
 interface body {
 	firstName: string;
@@ -11,8 +12,23 @@ interface body {
 const createAccount = async (req: Request<{}, {}, body>, res: Response) => {
 	try {
 		const { firstName, lastName, email, username, password } = req.body;
-		res.status(200).json(req.body);
-	} catch (error) {}
+		const salt = await bcrypt.genSalt(10);
+		const hashedPW = await bcrypt.hash(password, salt);
+		const newAccount = new Account({
+			firstName,
+			lastName,
+			email,
+			username,
+			hashedPW,
+		});
+		await newAccount.save();
+		res
+			.status(200)
+			.json({ message: "Successfully created a new account, please log in" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: error.message });
+	}
 };
 
 export { createAccount };
